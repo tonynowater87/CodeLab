edc433b5b31e4d8eeaeac9995b1a489302a829c0
tonynowater <tonynowater87@gmail.com>
2019-04-03 17:24:59 +0800

    CodeLab - Android Create Hello-CMake

---
+++ Android/hello-cmake/app/src/main/java/com/tonynowater/hello_cmake/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.tonynowater.hello_cmake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("DEBUG", "onCreate")
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            Log.d("DEBUG", "init loadLibrary start")
+            System.loadLibrary("native-lib")
+            Log.d("DEBUG", "init loadLibrary end")
+        }
+    }
+}
