9c61fc2e601abe11ea6094b146746d300beefa43
tonynowater <tonynowater87@gmail.com>
2019-04-03 12:20:23 +0800

    CodeLab - Android Navigation - 6. Changing the Navigation Transition

--- Android/android-navigation/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ Android/android-navigation/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -22,6 +22,7 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -39,25 +40,27 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
         val button = view.findViewById<Button>(R.id.navigate_destination_button)
         //切換頁面，第一種方式
         button?.setOnClickListener { findNavController().navigate(R.id.flow_step_one_dest) }
         //切換頁面，第二種方式
         button?.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.flow_step_one_dest, null))
+        //STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
 
-        //TODO STEP 6 - Set NavOptions
-//        val options = navOptions {
-//            anim {
-//                enter = R.anim.slide_in_right
-//                exit = R.anim.slide_out_left
-//                popEnter = R.anim.slide_in_left
-//                popExit = R.anim.slide_out_right
-//            }
-//        }
-//        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null, options)
-//        }
-        //TODO END STEP 6
+        //STEP 6 - Set NavOptions
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
+        //END STEP 6
 
         //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
 //        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
